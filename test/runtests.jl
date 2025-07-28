bc26816af26c526459c5030443b073ec647065b7
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-07-28 15:18:29 +0200

    Create first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,68 @@
 using DataFrames
-using XLSX
+using JuMP
 using TulipaBuilder
+using TulipaEnergyModel
 using Test
 
 @testset "TulipaBuilder.jl" begin
     tulipa = TulipaData()
-    add_asset!(tulipa, :ccgt, :producer, capacity = 225)
+
+    ### assets
+    add_asset!(tulipa, :ccgt, :producer, capacity = 2.0, investment_method = "simple")
+    attach_milestone_data!(tulipa, :ccgt, 2030, investable = true)
+    attach_commission_data!(tulipa, :ccgt, 2030, investment_cost = 3.0)
+
     add_asset!(
         tulipa,
         :solar,
         :producer,
-        description = "Stuff",
-        capacity = 10.0,
+        description = "Solar",
+        capacity = 1.0,
         resolution = 6,
     )
-    add_asset!(tulipa, :ocgt, :producer, capacity = 25.0, investment_method = "simple")
+    attach_both_years_data!(tulipa, :solar, 2030, 2030, initial_units = 10)
+
+    add_asset!(tulipa, :ocgt, :producer, capacity = 3.0, investment_method = "simple")
+    attach_milestone_data!(tulipa, :ocgt, 2030, investable = true)
+    attach_commission_data!(tulipa, :ocgt, 2030, investment_cost = 4.0)
 
     add_asset!(tulipa, :demand, :consumer)
+    attach_milestone_data!(tulipa, :demand, 2030, peak_demand = 30.0)
 
-    add_flow!(tulipa, :solar, :demand, capacity = 10.0)
+    ### flows
+    add_flow!(tulipa, :solar, :demand)
     for asset in (:ccgt, :ocgt)
-        add_flow!(tulipa, asset, :demand, capacity = 5.0)
+        add_flow!(tulipa, asset, :demand)
     end
 
-    xls = XLSX.readxlsx(joinpath(@__DIR__, "tulipatest.xlsx"))
-    profiles_df = DataFrame(XLSX.gettable(xls["profiles"]))
+    ### temporary manual basic rows creation
+    attach_commission_data!(tulipa, :solar, 2030)
+    attach_commission_data!(tulipa, :demand, 2030)
+    attach_milestone_data!(tulipa, :solar, 2030)
+    attach_both_years_data!(tulipa, :ccgt, 2030, 2030)
+    attach_both_years_data!(tulipa, :ocgt, 2030, 2030)
+    attach_both_years_data!(tulipa, :demand, 2030, 2030)
+    attach_commission_data!(tulipa, :ccgt, :demand, 2030)
+    attach_commission_data!(tulipa, :solar, :demand, 2030)
+    attach_commission_data!(tulipa, :ocgt, :demand, 2030)
+    attach_milestone_data!(tulipa, :ccgt, :demand, 2030)
+    attach_milestone_data!(tulipa, :solar, :demand, 2030)
+    attach_milestone_data!(tulipa, :ocgt, :demand, 2030)
 
-    attach_profile!(tulipa, :solar, :availability, profiles_df[!, "Solar"])
-
-    attach_profile!(tulipa, :demand, :demand, profiles_df[!, "Demand"])
-
-    attach_profile!(tulipa, :ccgt, :availability, 0.5 .+ 0.1 * randn(24))
-
+    ### profiles
+    domain = range(0.0, 1.0, length = 24)
+    attach_profile!(tulipa, :solar, :availability, 2030, 4 * domain .* (1 .- domain))
+    attach_profile!(tulipa, :demand, :demand, 2030, 0.5 .+ 0.1 * randn(24))
+    attach_profile!(tulipa, :ccgt, :availability, 2030, rand(0.1:0.1:0.9, 24))
     # no profile for ocgt
+
+    connection = create_connection(tulipa)
+
+    # External
+    ep = TulipaEnergyModel.run_scenario(
+        connection,
+        show_log = false,
+        model_file_name = "model.lp",
+    )
+    @test JuMP.is_solved_and_feasible(ep.model)
 end
