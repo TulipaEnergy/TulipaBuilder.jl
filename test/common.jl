3ce162b024e85f2e7eb5101c8c77e285f6bf7c54
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-12-03 14:13:14 +0100

    [WIP] Start working on implementing the Tiny example

--- test/common.jl
+++ test/common.jl
@@ -5,4 +5,28 @@
     using JuMP: JuMP
     using TulipaClustering: TulipaClustering as TC
     using TulipaEnergyModel: TulipaEnergyModel as TEM
+    using XLSX: XLSX
+
+    function get_vector_from_duckdb_query(connection, query)
+        return [row[1] for row in DuckDB.query(connection, query)]
+    end
+
+    function compare_duckdb_tables(connection, actual_table_name, expected_table_name)
+        table_names = Dict("actual" => actual_table_name, "expected" => expected_table_name)
+        column_names = Dict(
+            key => get_vector_from_duckdb_query(
+                connection,
+                "SELECT column_name FROM duckdb_columns() WHERE table_name = '$table_name'",
+            ) for (key, table_name) in table_names
+        )
+        @test Set(column_names["actual"]) == Set(column_names["expected"])
+
+        num_rows = Dict(
+            key => get_vector_from_duckdb_query(
+                connection,
+                "SELECT COUNT(*) FROM $table_name",
+            )[1] for (key, table_name) in table_names
+        )
+        @test num_rows["actual"] == num_rows["expected"]
+    end
 end
