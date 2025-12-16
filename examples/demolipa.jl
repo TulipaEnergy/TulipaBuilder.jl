60c110265c7f4b770c3fe0813190f49abc66c518
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-12-16 16:16:00 +0100

    Update demolipa example

--- examples/demolipa.jl
+++ examples/demolipa.jl
@@ -1,11 +1,10 @@
 # Used to create the rolling horizon test case
-using TulipaClustering
-using TulipaEnergyModel
-# using TulipaIO
-using TulipaBuilder
-using YAML
 using CSV
 using DataFrames
+using TulipaBuilder
+using TulipaClustering
+using TulipaEnergyModel
+using YAML
 
 # We are using TulipaBuilder as main development tool because we need RP = 1
 
@@ -17,7 +16,7 @@ capacity_factors_file = joinpath(data_path, "capacity-factors.csv")
 
 generators = readdir(generator_config_path)
 config_dicts = Dict()
-for fname in generators 
+for fname in generators
     config_full = YAML.load_file(joinpath(generator_config_path, fname))
     config = Dict()
     config["capacity"] = config_full["Generators"]["installed_capacity"]
@@ -28,20 +27,25 @@ for fname in generators
 end
 
 for (asset_name, config) in config_dicts
-    add_asset!(tulipa, asset_name, :producer; capacity = config["capacity"], initial_units = 0.0)
+    add_asset!(
+        tulipa,
+        asset_name,
+        :producer;
+        capacity = config["capacity"],
+        initial_units = 1.0,
+    )
 end
 
-add_asset!(
-    tulipa,
-    "bid_manager",
-    :consumer;
-    peak_demand = 0.0,
-    consumer_balance_sense = "==",
-)
+add_asset!(tulipa, "bid_manager", :consumer; peak_demand = 0.0)
 
 ### flows
 for (asset_name, config) in config_dicts
-    add_flow!(tulipa, asset_name, "bid_manager", operational_cost = config["operational_cost"])
+    add_flow!(
+        tulipa,
+        asset_name,
+        "bid_manager",
+        operational_cost = config["operational_cost"],
+    )
 end
 
 # ### profiles
@@ -49,15 +53,20 @@ df = DataFrame(CSV.File(capacity_factors_file))
 
 for (asset_name, config) in config_dicts
     profile = df[!, config["profile_name"]]
+    profile = profile / maximum(profile)
     attach_profile!(tulipa, asset_name, :availability, 2030, profile)
 end
 
 connection = create_connection(tulipa)
 
+TulipaBuilder.create_case_study_csv_folder(connection, "demolipa/pre-cluster")
+
 ### clustering
 dummy_cluster!(connection)
 
-### populate_with_defaults
-# populate_with_defaults!(connection)
+TulipaBuilder.create_case_study_csv_folder(connection, "demolipa/pre-populate")
 
-TulipaBuilder.create_case_study_csv_folder(connection, "demolipa")
+### populate_with_defaults
+populate_with_defaults!(connection)
+
+TulipaBuilder.create_case_study_csv_folder(connection, "demolipa/full")
